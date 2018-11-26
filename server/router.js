6c0ae22c703e5139b59d03ad196b8ad0a3031c48
Andrey Barkov <andrey@barkov.ca>
2018-11-25 20:39:31 -0500

    Fix return, implement max limit to avoid memory leak

--- server/router.js
+++ server/router.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const PrimesHandler = require('./PrimesHandler');
-
+const MAX_LIMIT = 100000000;
 router.post('/api/primesMedian', (req, res) => {
     const limit = parseInt(req.body.limit, 10);
-    if (!limit) {
+    if (!limit || limit< 2 || limit >= MAX_LIMIT) {
         res.sendStatus(400)
+        res.send('Limit is invalid')
         return;
     }
-    return PrimesHandler.findPrimesMedian(limit)
+    PrimesHandler.findPrimesMedian(limit)
     .then(median => {
-        res.send(median);
+        res.send({median});
     })
     .catch((error)=>{
         res.status(500);
