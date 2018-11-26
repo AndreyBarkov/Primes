92514c35d49b88c5c46f86e9ccb22567c7dc5c9c
Andrey Barkov <andrey@barkov.ca>
2018-11-25 21:38:50 -0500

    Fix router tests

--- tests/server/router_test.js
+++ tests/server/router_test.js
@@ -6,13 +6,18 @@ const PrimesHandler = require('../../server/PrimesHandler');
 
 describe('router', () => {
     let instance;
+    let sandbox;
+    let primesMedianStub
     before(() => {
-        instance = server.listen(5000);
+        instance = server.listen(3000);
+        sandbox = sinon.sandbox.create();
+        primesMedianStub = sandbox.stub(PrimesHandler, 'findPrimesMedian');
     })
     after(() => {
         instance.close();
+        sandbox.restore();
     })
-    let primesMedianStub = sinon.stub(PrimesHandler, 'findPrimesMedian');
+
     describe('default route', () => {
         it('returns test', (done) => {
             request(server).get('/').expect('Test', done)
@@ -21,13 +26,14 @@ describe('router', () => {
     })
     describe('/api/primesMedian route', () => {
         describe('request receives valid parameters', () => {
+           
             it('returns Primes median', (done) => {
                 primesMedianStub.resolves([3, 5]);
                 request(server).post('/api/primesMedian')
                     .send({ limit: 10 })
                     .set('Accept', 'application/json')
                     .expect(200, (err, res) => {
-                        expect(res.body).to.be.eql([3, 5])
+                        expect(res.body).to.be.eql({median:[3, 5]})
                     })
                 done();
             })
@@ -44,12 +50,22 @@ describe('router', () => {
             })
         })
         describe('request receives invalid parameters', () => {
-            it('returns 400 error', (done) => {
+            it('returns 400 error if less than lowest prime number', (done) => {
                 request(server).post('/api/primesMedian')
                     .send({ limit: 0 })
-                    .set('Accept', 'application/json')
+                    .expect(400, done)
+            })
+            it('returns 400 error if no limit value', (done) => {
+                request(server).post('/api/primesMedian')
+                    .send({ limit: '' })
+                    .expect(400, done)
+            })
+            it('returns 400 error if more than maximum limit value', (done) => {
+                const MAX_LIMIT = 100000000;
+                request(server).post('/api/primesMedian')
+                    .send({ limit: MAX_LIMIT+1})
                     .expect(400, done)
             })
         })
     })
-})
+}) 
